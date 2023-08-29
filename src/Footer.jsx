7f66759c74f7486df9d4aa629e939736632f2a0d
Habib <109099706+Habib-0007@users.noreply.github.com>
2023-08-29 17:36:15 +0100

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,16 +1,16 @@
 let Footer = () => {
   return (
     <footer className="mx-auto flex justify-between items-center flex-row p-4 w-72">
-      <a href="#" class="text-blue-500 text-3xl">
+      <a href="#https://www.facebook.com/habibopeyemi.adebayo" class="text-blue-500 text-3xl">
         <i class="fab fa-facebook"></i>
       </a>
-      <a href="#" class="text-sky-400 text-3xl">
+      <a href="https://twitter.com/Habib__001" class="text-sky-400 text-3xl">
         <i class="fab fa-twitter"></i>
       </a>
-      <a href="#" class="text-green-500 text-3xl">
+      <a href="https://wa.me/+2349019166356" class="text-green-500 text-3xl">
         <i class="fab fa-whatsapp"></i>
       </a>
-      <a href="#" class="text-black text-3xl">
+      <a href="https://www.github.com/Habib-0007" class="text-black text-3xl">
         <i class="fab fa-github"></i>
       </a>
     </footer>
